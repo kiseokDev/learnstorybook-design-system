3ce60a403a8f56d691f48c71c4433d5396f6b517
kiseokDev <dev.leekiseok@gmail.com>
2023-11-14 16:14:18 +0900

    undo button change

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -51,11 +51,9 @@ const StyledButton = styled.button`
   margin: 0;
   background: transparent;
 
-  /* border: 0;
+  border: 0;
   font-size: ${(props) =>
-    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px; */
-  border: 15px solid red;
-  font-size: 20px;
+    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
